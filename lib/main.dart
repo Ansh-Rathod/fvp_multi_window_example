48eda406189305d6df784b958a5b913094f88050
Ansh Rathod <anshrathod29@gmail.com>
2024-07-26 22:08:43 +0530

    made button to the center

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fvp/fvp.dart' as fvp;
 import 'package:video_player/video_player.dart';
@@ -49,14 +48,15 @@ class MainApp extends StatelessWidget {
 
               if (result != null) {
                 createNewWindow(
-                  size: const Size(300, 300),
+                  size: const Size(500, 500),
                   title: 'Test',
                   type: 'video_player',
                   args: {'inputPath': result.files.first.path},
                 );
               }
             },
-            child: const Text("Pick file and create new window")),
+            child:
+                const Center(child: Text("Pick file and create new window"))),
       ),
     );
   }
@@ -95,8 +95,10 @@ class VideoPlayerWindow extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: _VideoPlayer(
-          inputPath: arguments['inputPath'],
+        body: Center(
+          child: _VideoPlayer(
+            inputPath: arguments['inputPath'],
+          ),
         ),
       ),
     );
@@ -126,7 +128,17 @@ class _VideoPlayerState extends State<_VideoPlayer> {
       setState(() {});
     });
     _controller.setLooping(true);
+    _controller.initialize().then((value) {
+      setState(() {});
+      _controller.play();
+    });
     _controller.play();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -135,31 +147,99 @@ class _VideoPlayerState extends State<_VideoPlayer> {
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        FutureBuilder(
-          future: _controller.initialize(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return SizedBox(
-                width: 200,
-                height: 200,
-                child: Center(
-                  child: AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    child: Stack(
-                      alignment: Alignment.bottomCenter,
-                      children: <Widget>[
-                        VideoPlayer(_controller),
-                        VideoProgressIndicator(_controller,
-                            allowScrubbing: true),
-                      ],
+        SizedBox(
+          width: 500,
+          height: 500,
+          child: Center(
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  VideoPlayer(_controller),
+                  _ControlsOverlay(controller: _controller),
+                  VideoProgressIndicator(_controller, allowScrubbing: true),
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _ControlsOverlay extends StatelessWidget {
+  const _ControlsOverlay({required this.controller});
+
+  static const List<double> _examplePlaybackRates = <double>[
+    0.25,
+    0.5,
+    1.0,
+    1.5,
+    2.0,
+    3.0,
+    5.0,
+    10.0,
+  ];
+
+  final VideoPlayerController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        AnimatedSwitcher(
+          duration: const Duration(milliseconds: 50),
+          reverseDuration: const Duration(milliseconds: 200),
+          child: controller.value.isPlaying
+              ? const SizedBox.shrink()
+              : Container(
+                  color: Colors.black26,
+                  child: const Center(
+                    child: Icon(
+                      Icons.play_arrow,
+                      color: Colors.white,
+                      size: 100.0,
+                      semanticLabel: 'Play',
                     ),
                   ),
                 ),
-              );
-            }
-            return const Center(child: CupertinoActivityIndicator());
+        ),
+        GestureDetector(
+          onTap: () {
+            controller.value.isPlaying ? controller.pause() : controller.play();
           },
-        )
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: PopupMenuButton<double>(
+            initialValue: controller.value.playbackSpeed,
+            tooltip: 'Playback speed',
+            onSelected: (double speed) {
+              controller.setPlaybackSpeed(speed);
+            },
+            itemBuilder: (BuildContext context) {
+              return <PopupMenuItem<double>>[
+                for (final double speed in _examplePlaybackRates)
+                  PopupMenuItem<double>(
+                    value: speed,
+                    child: Text('${speed}x'),
+                  )
+              ];
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                // Using less vertical padding as the text is also longer
+                // horizontally, so it feels like it would need more spacing
+                // horizontally (matching the aspect ratio of the video).
+                vertical: 12,
+                horizontal: 16,
+              ),
+              child: Text('${controller.value.playbackSpeed}x'),
+            ),
+          ),
+        ),
       ],
     );
   }
